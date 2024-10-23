75be9aa0ea23d03e0738f3425adab545e15c9f0e
cmdiego <42076222+cmdiego@users.noreply.github.com>
2024-10-23 11:14:00 -0400

    Testing build

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,28 @@
 #include "raylib.h"
 
+#include <iostream>
+
+using namespace std;
+
+const struct
+{
+    int width = 800;
+    int height = 700;
+} windowScreen;
+
 int main()
 {
-    return 0;
+    InitWindow(windowScreen.width, windowScreen.height, "Hello, warrior!");
+
+    SetTargetFPS(60);
+
+    // Game loop
+    while(!WindowShouldClose())
+    {
+        BeginDrawing();
+        ClearBackground(BLACK);
+        EndDrawing();
+    }
+
+    CloseWindow();
 }
